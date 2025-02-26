88a580c55cb2824830f0e38b392ba977ede22cbf
ginsengAttack <howard20020225@gmail.com>
2025-02-26 17:23:08 +0800

    Update queue insert head
    Using duplicate function to allocate new space for node.
    And using list add to insert node in head.
    
    Change-Id: I01f815b4530bad3402a996d1a02d5d5fb9b0ddff

--- queue.c
+++ queue.c
@@ -34,6 +34,11 @@ void q_free(struct list_head *head)
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+    element_t *node = malloc(sizeof(element_t));
+    node->value = strdup(s);
+    list_add(&node->list, head);
     return true;
 }
 
