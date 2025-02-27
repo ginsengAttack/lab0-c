dd22c701176f34a9395711cd3e8305cf5dfe4bde
ginsengAttack <howard20020225@gmail.com>
2025-02-27 14:06:52 +0800

    Implement queue remove head and queue remove tail
    Record the element which want to remove,and copy node value in string.
    Finally remove the first node and last node.
    
    Change-Id: Iedee7a4046a009ee87c95858246c513a5540e12f

--- queue.c
+++ queue.c
@@ -57,13 +57,23 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || head->next == head)
+        return NULL;
+    element_t *node = list_entry(head->next, element_t, list);
+    strncpy(sp, node->value, bufsize);
+    list_del(&node->list);
+    return node;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || head->next == head)
+        return NULL;
+    element_t *node = list_entry(head->prev, element_t, list);
+    strncpy(sp, node->value, bufsize);
+    list_del(&node->list);
+    return node;
 }
 
 /* Return number of elements in queue */
